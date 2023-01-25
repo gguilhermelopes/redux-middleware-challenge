619354ace718ab2fa21f858b340727069fc88a41
gguilhermelopes <guilhermelopes275@gmail.com>
2023-01-25 15:46:20 -0300

    token criado

--- store/token.js
+++ store/token.js
@@ -1,3 +1,15 @@
+const TOKEN_FETCH_STARTED = "token/FETCH_STARTED";
+const TOKEN_FETCH_SUCCESS = "token/FETCH_SUCCESS";
+const TOKEN_FETCH_ERROR = "token/FETCH_ERROR";
+
+const tokenFetchStarted = () => ({ type: TOKEN_FETCH_STARTED });
+const tokenFetchSuccess = (payload) => ({
+  type: TOKEN_FETCH_STARTED,
+  payload,
+  localStorage: "token",
+});
+const tokenFetchError = (payload) => ({ type: TOKEN_FETCH_STARTED, payload });
+
 function token(state = 0, action) {
   switch (action.type) {
     case "INCREMENTAR":
